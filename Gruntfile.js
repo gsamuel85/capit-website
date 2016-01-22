6df35e7e3be0cb2f62c912f78dcba241aa4f9a43
Guy Samuel <gsamuel85@gmail.com>
2016-01-22 09:57:41 +0000

    Initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Load tasks from Grunt dependencies
+    require("matchdep").filterAll("grunt-*").forEach(grunt.loadNpmTasks);
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        app_path: 'src',
+        dist_path: 'dist',
+        
+        // HTML
+        htmlhint: {
+            build: {
+                options: {
+                    'tag-pair': true,
+                    'tagname-lowercase': true,
+                    'attr-lowercase': true,
+                    'attr-value-double-quotes': true,
+                    'spec-char-escape': true,
+                    'id-unique': true,
+                    'head-script-disabled': true,
+                    'style-disabled': true
+                },
+                src: ['<%= app_path %>/*.html']
+            }
+        },
+        
+        
+        // JAVASCRIPT
+        jshint: {
+            source: {
+                options: {
+                    "browser": true,
+                    "curly": true,
+                    "eqnull": true,
+                    "eqeqeq": true,
+                    "globalstrict": true, "strict": false,
+                    "jquery": true,
+                    "latedef": true,
+                    "undef": true,
+                    "devel": true
+                },
+                src: ["<%= app_path %>/js/main.js"]
+            }
+        },
+        uglify: {
+            source: {
+                files: {
+                    '<%= dist_path %>/js/main.min.js': '<%= app_path %>/js/main.js'
+                }
+            }
+        },
+        
+        
+        // CSS
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    '<%= app_path %>/sass/**/*.scss': '<%= app_path %>/css/main.scss'
+                }
+            }
+        },
+        cssmin: {
+            source: {
+                files: {
+                    '<%= dist_path %>/css/main.min.css': '<%= app_path %>/css/main.css'
+                }
+            }
+        },
+        
+        
+        watch: {
+            html: {
+                files: ['<%= app_path %>/*.html'],
+                tasks: ['htmlhint']
+            },
+            js: {
+                files: ['<%= app_path %>/js/**/*.js'],
+                tasks: ['jshint']
+            },
+            scss: {
+                files: ['<%= app_path %>/sass/**/*.scss'],
+                tasks: ['sass']
+            }
+        }
+        
+        
+    });
+    
+    grunt.registerTask('js', ['jshint', 'uglify']);
+    grunt.registerTask('css', ['sass', 'cssmin']);
+    
+    grunt.registerTask('default', ['htmlhint', 'js', 'css']);
+    
+};
