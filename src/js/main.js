0b918a50684a0b1e88f6efc01a8f88260b47cf37
Guy Samuel <gsamuel85@gmail.com>
2016-02-14 10:26:51 +0000

    add contact form - Insightly

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,24 @@ var stickMenu = function stickMenu() {
 window.addEventListener("scroll", function(e) {
 	stickMenu();
 });
+
+
+// Handle form submission
+var form = document.getElementById("contact-form");
+form.addEventListener("submit", function submitContact(e) {
+	e.preventDefault();
+	
+	var contactData = new FormData();
+	contactData.append("formId", "wRKKzTKy5PoyLATiwmQi4A==");
+	contactData.append("emails[0].Label", "Work");
+	contactData.append("email[0]_Value", document.getElementById("email").value);
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	  if (xhttp.readyState === 4 && xhttp.status === 200) {
+	    console.log(xhttp.responseText);
+	  }
+	};
+	xhttp.open("POST", "https://googleapps.insight.ly/WebToContact/Create", true);
+	xhttp.send(contactData);
+});
